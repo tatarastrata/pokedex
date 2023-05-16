42f275228ee57bfb76f81237023d0a893e0b1d0b
tamara <22053528+tatarastrata@users.noreply.github.com>
2023-05-16 21:54:20 +0300

    gh-pages

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './src/App';
+import { App } from './App';
 
 const root = document.getElementById('root');
 
--- plopfile.js
+++ plopfile.js
@@ -23,32 +23,32 @@ module.exports = function (plop) {
     actions: [
       {
         type: 'add',
-        path: 'src/components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        path: './components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
         templateFile: 'templates/Component.tsx.hbs',
       },
       {
         type: 'add',
-        path: 'src/components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}.module.scss',
+        path: './components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}.module.scss',
         templateFile: 'templates/Component.module.ts.hbs',
       },
       {
         type: 'add',
-        path: 'src/components/{{dir}}/{{pascalCase name}}/index.ts',
+        path: './components/{{dir}}/{{pascalCase name}}/index.ts',
         templateFile: 'templates/index.ts.hbs',
       },
       {
         type: 'add',
-        path: 'src/components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}PropTypes.ts',
+        path: './components/{{dir}}/{{pascalCase name}}/{{pascalCase name}}PropTypes.ts',
         templateFile: 'templates/ComponentPropTypes.ts.hbs',
       },
       {
         type: 'add',
-        path: 'src/components/{{dir}}/index.ts',
+        path: './components/{{dir}}/index.ts',
         skipIfExists: true,
       },
       {
         type: 'append',
-        path: 'src/components/{{dir}}/index.ts',
+        path: './components/{{dir}}/index.ts',
         separator: '',
         template: `export { default as {{pascalCase name}} } from './{{pascalCase name}}';\n`,
         unique: true,
