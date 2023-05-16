aaf4dec59d4e5359ea6768d8e6bf6fa4b2970ccc
tamara <22053528+tatarastrata@users.noreply.github.com>
2023-05-16 22:14:20 +0300

    gh-pages

--- index.tsx
+++ index.tsx
@@ -1,5 +1,19 @@
+import { IndexPage } from './components/pages';
+import './styles.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+
+export const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <IndexPage />
+      </ChakraProvider>
+    </Provider>
+  );
+};
 
 const root = document.getElementById('root');
 
